3f6d4124956773cd4f6f8aefe5e9e26ceab46d7f
신재훈 <shinjaehoon@sinjaehun-ui-MacBookPro.local>
2022-07-05 19:42:22 +0900

    seventh

--- includes/db_linked_list.h
+++ includes/db_linked_list.h
@@ -6,7 +6,7 @@
 /*   By: jshin <jshin@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 20:31:41 by jshin             #+#    #+#             */
-/*   Updated: 2022/07/04 04:10:19 by jshin            ###   ########.fr       */
+/*   Updated: 2022/07/04 16:26:41 by jshin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ void	stack_push_back(t_stack *list, int data)
 
 int	stack_peek(t_stack *stack)
 {
-	if (!stack || !stack->head)
+	if (!stack || (stack->num_node <= 0))
 		return -1;
 	return stack->head->val;
 }
@@ -76,7 +76,7 @@ void	stack_pop(t_stack *stack)
 {
 	t_node	*temp;
 
-	if (!stack || !stack->head)
+	if (!stack || (stack->num_node <= 0))
 		return ;
 	temp = stack->head;
 	stack->head = stack->head->next;
@@ -86,15 +86,21 @@ void	stack_pop(t_stack *stack)
 		stack->tail = NULL;
 	}
 	else
-		stack->cur = stack->head->next;
+		stack->cur = stack->head;
 	free(temp);
 	stack->num_node -= 1;
 }
 
-void	stack_push(t_stack *stack, t_node *new_node)
+void	stack_push(t_stack *stack, int data)
 {
-	if (!stack || !new_node || !stack->head)
+	t_node *new_node;
+
+	if (!stack)
 		return ;
+	new_node = (t_node *)malloc(sizeof(t_node));
+	if (!new_node)
+		return ;
+	new_node->val = data;
 	new_node->next = stack->head;
 	if (stack->head)
 		stack->head->pre = new_node;
@@ -106,31 +112,128 @@ void	stack_push(t_stack *stack, t_node *new_node)
 
 void	pa(t_stack *a, t_stack *b)
 {
-	t_node	*new_node;
+	int	temp;
 
 	if (!a || !b)
 		return ;
-	new_node = (t_node *)malloc(sizeof(t_node));
-	if (!new_node)
+	temp = stack_peek(b);
+	if (temp == -1)
 		return ;
-	new_node->val = stack_peek(b);
 	stack_pop(b);
-	stack_push(a, new_node);
+	stack_push(a, temp);
 }
 
 void	pb(t_stack *a, t_stack *b)
 {
-	t_node	*new_node;
+	int	temp;
+
+	if (!b || !a)
+		return ;
+	temp = stack_peek(a);
+	if (temp == -1)
+		return ;
+	stack_pop(a);
+	stack_push(b, temp);
+}
+
+void	sa(t_stack *a)
+{
+	int	temp;
+
+	if (!a || (a->num_node <= 1))
+		return ;
+	temp = a->head->val;
+	a->head->val = a->head->next->val;
+	a->head->next->val = temp;
+}
+
+void	sb(t_stack *b)
+{
+	int	temp;
+
+	if (!b || (b->num_node <= 1))
+		return ;
+	temp = b->head->val;
+	b->head->val = b->head->next->val;
+	b->head->next->val = temp;
+}
+
+void	ss(t_stack *a, t_stack *b)
+{
+	sa(a);
+	sb(b);
+}
+
+void	ra(t_stack *a)
+{
 	t_node	*temp;
 
-	if (!a || !b)
+	if (!a || (a->num_node <= 1))
 		return ;
-	new_node = (t_node *)malloc(sizeof(t_node));
-	if (!new_node)
+	temp = a->head;
+	a->head = a->head->next;
+	a->head->pre = NULL;
+	a->tail->next = temp;
+	temp->pre = a->tail->next;
+	a->tail = temp;
+	a->tail->next = NULL;
+}
+
+void	rb(t_stack *b)
+{
+	t_node	*temp;
+
+	if (!b || (b->num_node <= 1))
 		return ;
-	new_node->val = stack_peek(a);
-	stack_pop(a);
-	stack_push(b, new_node);
+	temp = b->head;
+	b->head = b->head->next;
+	b->head->pre = NULL;
+	b->tail->next = temp;
+	b->tail = temp;
+	b->tail->next = NULL;
+}
+
+void	rr(t_stack *a, t_stack *b)
+{
+	ra(a);
+	rb(b);
+}
+
+void	rra(t_stack *a)
+{
+	t_node	*temp;
+
+	if (!a || (a->num_node <= 1))
+		return ;
+	temp = a->tail;
+	a->tail = a->tail->pre;
+	temp->pre = NULL;
+	a->tail->next = NULL;
+	temp->next = a->head;
+	a->head->pre = temp;
+	a->head = temp;
+	a->head->pre = NULL;
+}
+
+void	rrb(t_stack *b)
+{
+	t_node	*temp;
+
+	if (!b || (b->num_node <= 1))
+		return ;
+	b->tail->pre = NULL;
+	temp = b->tail;
+	b->tail = b->tail->pre;
+	b->tail->next = NULL;
+	temp->next = b->head;
+	b->head->pre = temp;
+	b->head = temp;
+}
+
+void	rrr(t_stack *a, t_stack *b)
+{
+	rra(a);
+	rrb(b);
 }
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,37 @@
 /*   By: jshin <jshin@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 20:04:53 by jshin             #+#    #+#             */
-/*   Updated: 2022/07/04 07:25:17 by jshin            ###   ########.fr       */
+/*   Updated: 2022/07/04 16:26:43 by jshin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+void	print(t_stack *a, t_stack *b)
+{
+	printf("[a values] | [b values]\n");
+	printf("\n");
+	a->cur = a->head;
+	while (a->cur)
+	{
+		printf("a: %d\n", a->cur->val);
+		a->cur = a->cur->next;
+	}
+	printf("\n");
+	b->cur = b->head;
+	while (b->cur)
+	{
+		printf("b: %d\n", b->cur->val);
+		b->cur = b->cur->next;
+	}
+	printf("\n");
+	printf("a->num_node: %d\n", a->num_node);
+	printf("\n");
+	printf("b->num_node: %d\n", b->num_node);
+	printf("\n");
+	printf("-------------------------------------\n");
+	printf("\n");
+}
 
 int	main(int argc, char **argv)
 {
@@ -32,138 +58,22 @@ int	main(int argc, char **argv)
 			stack_push_back(&a, ft_atoi(temp[j++]));
 		i++;
 	}
-	stack_push_back(&b, 1004);
-	stack_push_back(&b, 1005);
-	printf("[a values]\n");
-	a.cur = a.head;
-	while (a.cur)
-	{
-		printf("a: %d\n", a.cur->val);
-		a.cur = a.cur->next;
-	}
-	printf("\n");
-	printf("[b values]\n");
-	b.cur = b.head;
-	while (b.cur)
-	{
-		printf("b: %d\n", b.cur->val);
-		b.cur = b.cur->next;
-	}
-	printf("\n");
-	printf("a.num_node: %d\n", a.num_node);
-	printf("\n");
-	printf("b.num_node: %d\n", b.num_node);
-	printf("\n");
-	printf("-------------------------------------\n");
-	printf("\n");
+
+	print(&a, &b);
+	rra(&a);
+	printf("rra();\n\n");
+	print(&a, &b);
+	pb(&a, &b);
+	printf("pb();\n\n");
+	print(&a, &b);
+	sa(&a);
+	printf("sa();\n\n");
+	print(&a, &b);
+	rra(&a);
+	printf("rra();\n\n");
+	print(&a, &b);
 	pa(&a, &b);
-	printf("pa()\n");
-	printf("\n");
-	printf("[a values]\n");
-	a.cur = a.head;
-	while (a.cur)
-	{
-		printf("a: %d\n", a.cur->val);
-		a.cur = a.cur->next;
-	}
-	printf("\n");
-	printf("[b values]\n");
-	b.cur = b.head;
-	while (b.cur)
-	{
-		printf("b: %d\n", b.cur->val);
-		b.cur = b.cur->next;
-	}
-	printf("\n");
-	printf("a.num_node: %d\n", a.num_node);
-	printf("\n");
-	printf("b.num_node: %d\n", b.num_node);
-	printf("\n");
-	printf("-------------------------------------\n");
-	printf("\n");
-
-	pb(&a, &b);
-	printf("pb()\n");
-	printf("\n");
-	printf("[a values]\n");
-	a.cur = a.head;
-	while (a.cur)
-	{
-		printf("a: %d\n", a.cur->val);
-		a.cur = a.cur->next;
-	}
-	printf("\n");
-	printf("[b values]\n");
-	b.cur = b.head;
-	while (b.cur)
-	{
-		printf("b: %d\n", b.cur->val);
-		b.cur = b.cur->next;
-	}
-	printf("\n");
-	printf("a.num_node: %d\n", a.num_node);
-	printf("\n");
-	printf("b.num_node: %d\n", b.num_node);
-	printf("\n");
-	printf("-------------------------------------\n");
-	printf("\n");
-
-	pb(&a, &b);
-	printf("pb()\n");
-	printf("\n");
-	printf("[a values]\n");
-	a.cur = a.head;
-	while (a.cur)
-	{
-		printf("a: %d\n", a.cur->val);
-		a.cur = a.cur->next;
-	}
-	printf("\n");
-	printf("[b values]\n");
-	b.cur = b.head;
-	while (b.cur)
-	{
-		printf("b: %d\n", b.cur->val);
-		b.cur = b.cur->next;
-	}
-	printf("\n");
-	printf("a.num_node: %d\n", a.num_node);
-	printf("\n");
-	printf("b.num_node: %d\n", b.num_node);
-	stack_pop(&a);
-	stack_pop(&a);
-	stack_pop(&a);
-	printf("stack_pop_a()\n");
-	printf("stack_pop_a()\n");
-	printf("stack_pop_a()\n");
-	printf("\n");
-	stack_pop(&b);
-	stack_pop(&b);
-	stack_pop(&b);
-	stack_pop(&b);
-	printf("stack_pop_b()\n");
-	printf("stack_pop_b()\n");
-	printf("stack_pop_b()\n");
-	printf("stack_pop_b()\n");
-	printf("\n");
-	printf("[a values]\n");
-	a.cur = a.head;
-	while (a.cur)
-	{
-		printf("a: %d\n", a.cur->val);
-		a.cur = a.cur->next;
-	}
-	printf("\n");
-	printf("[b values]\n");
-	b.cur = b.head;
-	while (b.cur)
-	{
-		printf("b: %d\n", b.cur->val);
-		b.cur = b.cur->next;
-	}
-	printf("\n");
-	printf("a.num_node: %d\n", a.num_node);
-	printf("\n");
-	printf("b.num_node: %d\n", b.num_node);
+	printf("pa();\n\n");
+	print(&a, &b);
 	return (0);
 }
